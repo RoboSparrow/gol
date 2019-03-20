79a994501528acf7ca7551596e852a4d317a2dd2
joerg <sendstuff2bo@gmail.com>
2019-03-20 15:51:10 +1030

    cleanup

--- src/main.c
+++ src/main.c
@@ -35,7 +35,7 @@ int main(int argc, char* argv[]) {
     // init world data
     world->data = gol_allocate_data(world->cols, world->rows);
     EXIT_NULL(world->data, "could not allocate memory(2) for world pattern");
-printf("==> %s\n", patternfile);
+
     // populate world
     if(patternfile[0] == '\0') {
         // random world
@@ -43,8 +43,8 @@ printf("==> %s\n", patternfile);
     } else {
         Pattern *pattern = pattern_allocate_pattern();
         EXIT_NULL(world, "main: could not allocate memory for pattern file\n");
-        int loaded = pattern_load_file(patternfile, pattern, PATTERN_FULL);
 
+        int loaded = pattern_load_file(patternfile, pattern, PATTERN_FULL);
         EXIT_MINUS(loaded, "main: could not load pattern file\n");
         gol_merge_data(pattern, world, (world->cols - pattern->cols) / 2, (world->rows - pattern->rows) / 2);
 
@@ -93,6 +93,7 @@ printf("==> %s\n", patternfile);
 
                                 int loaded = pattern_load_file(patternfile, pattern, PATTERN_FULL);
                                 EXIT_MINUS(loaded, "main: could not load pattern file\n");
+
                                 gol_merge_data(pattern, world, (world->cols - pattern->cols) / 2, (world->rows - pattern->rows) / 2);
                                 gol_free_data(pattern->data);
                                 pattern_free_pattern(pattern);
