a40e8c40439078393f8d669d21b1b6106bb53db3
joerg <sendstuff2bo@gmail.com>
2019-03-30 11:31:29 +1030

    state color globals and utils

--- include/state.h
+++ include/state.h
@@ -5,6 +5,8 @@
 #ifndef __STATE_H__
 #define __STATE_H__
 
+#include <SDL2/SDL.h>
+
 #define UNIT_SIZE 10
 #define APP_NAME "RoboSparrow's game of Life"
 #define FONT "assets/hellovetica.ttf"
@@ -14,4 +16,12 @@ typedef enum {
     SDL_SCREEN_WORLD
 } SdlScreen;
 
+typedef struct SdlColors {
+   SDL_Color bg;
+   SDL_Color fg;
+   SDL_Color grid;
+   SDL_Color text;
+} SdlColors;
+
+extern SdlColors Colors;
 #endif
---
+++ src/state.c
@@ -0,0 +1,10 @@
+#include <SDL2/SDL.h>
+
+#include "state.h"
+
+SdlColors Colors = {
+    .bg = { 44, 74, 82 }, // #2C4A52
+    .fg = { 244, 235, 219 }, // #F4EBDB
+    .grid ={ 10, 49, 57 }, // bg_color darkened 10%
+    .text ={ 134, 155, 151 }
+};
